e12a80f533216eebdf6894c4bf8864a128b9c8b2
belvisx <guilherme.belvis@gmail.com>
2025-03-09 17:16:37 -0300

    update_1

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,23 +1,27 @@
-
-import { bosses } from "@/lib/bosses"
-import { PhaseSidebar } from "../_components/phases"
-import { MapPin, Star } from 'lucide-react';
-import {useState} from 'react'
+import { bosses } from "@/lib/bosses";
+import { PhaseSidebar } from "../_components/phases";
+import { MapPin } from "lucide-react";
 import FavoriteButton from "../_components/favorite";
 
+interface BossPageParams {
+    id: string;
+}
 
 interface Props {
-    params: {
-        id: string;
-    };
+    params: BossPageParams;
+}
+
+export function generateStaticParams() {
+    return bosses.map((boss) => ({
+        id: boss.name.toLowerCase(),
+    }));
 }
 
 export default function BossPage({ params }: Props) {
-    const boss = bosses.find((b) => b.name.toLowerCase() === decodeURIComponent(params.id.toLowerCase()))
-
+    const boss = bosses.find((b) => b.name.toLowerCase() === decodeURIComponent(params.id.toLowerCase()));
 
     if (!boss) {
-        return <h1 className="text-center text-2xl font-bold mt-10">Boss não encontrado</h1>
+        return <h1 className="text-center text-2xl font-bold mt-10">Boss não encontrado</h1>;
     }
 
     return (
@@ -31,11 +35,11 @@ export default function BossPage({ params }: Props) {
                     <img className="w-80 h-80 object-cover rounded-lg mt-4" src={boss.img} alt={boss.name} />
                 </div>
                 <div className="flex flex-col text-center lg:text-left">
-                <div className="flex justify-center mb-12">
-                            <FavoriteButton boss={boss} />
-                        </div>
+                    <div className="flex justify-center mb-12">
+                        <FavoriteButton boss={boss} />
+                    </div>
                     <p className="text-lg">{boss.description}</p>
-                    <a className="text-lg font-semibold" href={boss.location} target="blank">
+                    <a className="text-lg font-semibold" href={boss.location} target="_blank">
                         <div className="flex justify-center items-center text-center mt-12 rounded-2xl bg-[#6C3232] p-2 gap-2">
                             <MapPin />
                             <span className="text-lg font-bold">Location</span>
